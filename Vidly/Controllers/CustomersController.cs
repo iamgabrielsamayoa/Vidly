c6b4ac1487cfcc6a57dadd29053cd18ce1657165
Gabriel <gabriel.samayoale@gmail.com>
2020-09-06 13:04:54 -0600

    1.2.1

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using System.Data.Entity;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            //Especificamos con include que atributo agregar
+            var customers = _context.customers.Include(c => c.MembershipType).ToList();
+
+            
+
+            return View(customers);
+        }
+
+        public ActionResult Details(int id)
+        {
+            var customer = _context.customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
--- Vidly/Models/customer.cs
+++ Vidly/Models/customer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,8 +8,10 @@ namespace Vidly.Models
 {
     public class customer
     {
+        //[] == Non nullable,varchar etc USUALLY SQL TABLE CREATION PROPERTIES
         public int Id { get; set; }
-
+        [Required]
+        [StringLength(255)]
         public String  Name { get; set; }
         public bool IsSuscribedToNewsLetter { get; set; }
 
